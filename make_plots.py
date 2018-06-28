94c1da3da5281acbcfae50879d17ddc4c001b0df
ron <ronimber@gmail.com>
2018-06-28 15:52:46 +0300

    .

---
+++ make_plots.py
@@ -0,0 +1,72 @@
+from scipy.signal import resample_poly, correlate, correlate2d, savgol_filter
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+from prep_data import *
+# %%
+# Lowpass 5Hz!!!
+sensor_loc = r"E:\data\18-06-26_data_for_hackton\Vesper Data\rat1_try2 - rat4_try1.csv"
+sensor_data = pd.read_csv(sensor_loc)
+times = np.array(sensor_data.iloc[:, 0])
+sensor_data = sensor_data.iloc[:,1:]
+sensor_data['time'] = [datetime.fromordinal(t.astype(int)) + timedelta(days=t%1) - timedelta(days = 366) for t in times]
+# sensor_data['time'] = np.arange(sensor_data.shape[0]) * (1 / 50)
+
+camera_loc = r"E:\data\18-06-26_data_for_hackton\Tracking Data\rat1_try2.csv"
+camera_data = pd.read_csv(camera_loc)
+
+data_loc = 'alldata.mat'
+start_ind = 800
+data = load_synced_data(data_loc, start_ind)
+target = make_target(data)
+features = make_features(data)
+# %% Raw data
+plt.figure(1)
+plt.subplot(121)
+plt.plot(camera_data[['X1','Y1','Z1']])
+plt.title('Tracker Data')
+plt.legend(['X', 'Y', 'Z'])
+plt.ylabel('Location')
+plt.xlabel('Sample #')
+plt.subplot(122)
+plt.plot(sensor_data[['x_acc','y_acc','z_acc']])
+plt.title('Accelerometer data')
+plt.ylabel('Acceleration')
+plt.xlabel('Sample #')
+plt.show()
+# %% Divergence
+fig, ax11 = plt.subplots()
+ax11.plot(sensor_data.x_acc, '-b')
+ax11.set_xlabel('Sample #')
+ax11.set_ylabel('Acceleration', color='b')
+ax11.set_title('Sensor X-Axis data')
+ax11.tick_params('y', colors='b')
+
+ax21 = ax11.twinx()
+ax21.plot(sensor_data.x_acc.cumsum(), '-r')
+ax21.set_ylabel('Velocity', color='r')
+ax21.tick_params('y', colors='r')
+# %% Sync dance
+plt.figure(3)
+plt.subplot(211)
+plt.title('Syncing')
+plt.plot(camera_data.TIME, camera_data['Z1'])
+plt.xlabel('Time (s)')
+plt.subplot(212)
+plt.plot(sensor_data.time[75000:250000], sensor_data.z_acc[75000:250000])
+plt.xlabel('Day + TOD')
+# %%
+fig, ax12 = plt.subplots()
+ax12.plot(features.acc_vec_magnitude, '-b')
+ax12.set_xlabel('Sample #')
+ax12.set_ylabel('Acceleration magnitude', color='b')
+ax12.set_title('Vector Data')
+ax12.tick_params('y', colors='b')
+
+ax22 = ax12.twinx()
+ax22.plot(target.magnitude, '-r')
+ax22.set_ylabel('Velocity magnitude', color='r')
+ax22.tick_params('y', colors='r')
+
+
